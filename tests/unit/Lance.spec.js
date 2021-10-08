7cde19eb444fdb34aa773b475ae4d512862c51cd
leticiamsfernandes <lemayara16@gmail.com>
2021-10-08 08:52:59 -0300

    Adicionando props

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -1,40 +1,75 @@
 import Lance from '@/components/Lance.vue';
 import { mount } from '@vue/test-utils';
 
-test('não aceita lance com valor menor do que zero', () => {
-  const wrapper = mount(Lance);
-  const input = wrapper.find('input');
+describe('um lance sem valor mínimo', () => {
+  test('não aceita lance com valor menor do que zero', () => {
+    const wrapper = mount(Lance);
+    const input = wrapper.find('input');
 
-  input.setValue(-100);
+    input.setValue(-100);
+    wrapper.trigger('submit');
 
-  const lancesEmitidos = wrapper.emitted('novo-lance');
+    const lancesEmitidos = wrapper.emitted('novo-lance');
+    expect(lancesEmitidos).toBeUndefined();
 
-  wrapper.trigger('submit');
-  expect(lancesEmitidos).toBeUndefined();
+  });
 
+  test('emite um lance quando o valor é maior do que zero', () => {
+    const wrapper = mount(Lance);
+    const input = wrapper.find('input');
+
+    input.setValue(100);
+    wrapper.trigger('submit');
+
+    const lancesEmitidos = wrapper.emitted('novo-lance');
+    expect(lancesEmitidos).toHaveLength(1);
+
+  });
+
+  test('emite um valor esperado de um lance válido', () => {
+    const wrapper = mount(Lance);
+    const input = wrapper.find('input');
+
+    input.setValue(100);
+    wrapper.trigger('submit');
+
+    const lancesEmitidos = wrapper.emitted('novo-lance');
+    const lance = parseInt(lancesEmitidos[0][0]);
+
+    expect(lance).toBe(100);
+  });
 });
 
-test('emite um lance quando o valor é maior do que zero', () => {
-  const wrapper = mount(Lance);
-  const input = wrapper.find('input');
+describe('um lance com valor mínimo', () => {
+  test('todos os lances devem possuir um valor maior do que o mínimo informado', () => {
+    const wrapper = mount(Lance, {
+      propsData: {
+        lanceMinimo: 300
+      }
+    });
+    const input = wrapper.find('input');
 
-  input.setValue(100);
-  wrapper.trigger('submit');
+    input.setValue(400);
+    wrapper.trigger('submit');
 
-  const lancesEmitidos = wrapper.emitted('novo-lance');
-  expect(lancesEmitidos).toHaveLength(1);
+    const lancesEmitidos = wrapper.emitted('novo-lance');
+    expect(lancesEmitidos).toHaveLength(1);
+  });
 
-});
+  test('emite um valor esperado de um lance válido', () => {
+    const wrapper = mount(Lance, {
+      propsData: {
+        lanceMinimo: 300
+      }
+    });
+    const input = wrapper.find('input');
 
-test('emite um valor esperado de um lance válido', () => {
-  const wrapper = mount(Lance);
-  const input = wrapper.find('input');
+    input.setValue(400);
+    wrapper.trigger('submit');
 
-  input.setValue(100);
-  wrapper.trigger('submit');
+    const lancesEmitidos = wrapper.emitted('novo-lance');
+    const valorDoLance = parseInt(lancesEmitidos[0][0]);
 
-  const lancesEmitidos = wrapper.emitted('novo-lance');
-  const lance = parseInt(lancesEmitidos[0][0]);
-  
-  expect(lance).toBe(100);
+    expect(valorDoLance).toBe(400);
+  });
 });
